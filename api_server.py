b4e8a12fd71a8ddc5f7da894b3f3d1e024436b75
Ubuntu <ubuntu@ip-172-31-35-173.ec2.internal>
2024-09-23 17:26:54 +0000

    update

--- api_server.py
+++ api_server.py
@@ -1,4 +1,4 @@
-import json, redis, uuid, time
+import json, redis, uuid, time, config
 from flask import Flask, jsonify, request
 from utils import auth_req
 from llm import generate_completion
@@ -58,4 +58,4 @@ def score_coupon(context, coupon_text):
         return 0.0
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=8050)
+    app.run(debug=True, host='0.0.0.0', port=config.API_PORT)
